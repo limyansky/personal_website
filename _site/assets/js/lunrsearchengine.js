7494adfea9ac78f0e899f40168f5d56ff25051c3
Brent L <brent.limyansky@ucsc.edu>
2022-09-28 16:07:22 -0700

    adding css image captions

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "http://localhost:4000/about",
     "title": "About Me",
-    "body": "I am a recent graduate from the University of California, Santa Cruz, where I received my PhD in physics for my work studying the emission mechanism of pulsars. I have made this website primarily to showcase personal projects relevant to a career in data science, but will also be showcasing some of my more technically oriented hobbies. For more information on my career, including academic works, please visit my linkedIn. On the hobby side, I occasionally post photographs I’ve taken on flickr.  "
+    "body": "I am a recent graduate from the University of California, Santa Cruz, where I received my PhD in physics for my work studying the emission mechanism of pulsars. I have made this website primarily to showcase personal projects relevant to a career in data science, but will also be showcasing some of my more technically oriented hobbies. For more information on my career, including academic works, please visit my linkedIn. On the hobby side, I occasionally post photographs I’ve taken on flickr. A small celebration after my thesis defense. "
     }, {
     "id": 2,
     "url": "http://localhost:4000/categories",
